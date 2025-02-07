6748b1e343bf7dd75bc68535368f6c4644e80791
Somondas <somondas067@gmail.com>
2025-02-07 22:06:05 +0530

    Updates

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import express from "express";
 import http from "http";
 import path from "path";
+import { Server as SocketIO } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
+const io = new SocketIO(server);
 
 app.use(express.static(path.resolve("./public")));
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+  socket.on("binarystream", (data) => {
+    console.log("Received binary data:", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
 
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
--- public/script.js
+++ public/script.js
@@ -1,8 +1,24 @@
 const userVideo = document.getElementById("user-video");
+const startBtn = document.getElementById("start-btn");
+
+const state = { media: null };
+const socket = io();
+startBtn.addEventListener("click", () => {
+  const mediaRecorder = new MediaRecorder(state.media, {
+    audioBitsPerSecond: 128000,
+    videoBitsPerSecond: 2500000,
+    framerate: 25,
+  });
+  mediaRecorder.ondataavailable = (e) => {
+    socket.emit("binarystream", e.data);
+  };
+  mediaRecorder.start(25);
+});
 window.addEventListener("load", async (e) => {
   const media = await navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true,
   });
+  state.media = media;
   userVideo.srcObject = media;
 });
